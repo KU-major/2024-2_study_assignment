02ae102bb08972548f04f3c682cf9af77c8b2150
KU-yohan <yohan8628@korea.ac.kr>
2024-12-28 15:30:49 +0900

    Update 4_1_calculator_ans.cs

--- assignment4/4_1_calculator_ans.cs
+++ assignment4/4_1_calculator_ans.cs
@@ -56,7 +56,9 @@ namespace calculator
                 case "+": return num1 + num2;
                 case "-": return num1 - num2;
                 case "*": return num1 * num2;
-                case "/": return num1 / num2;
+                case "/":
+                    if(num2 != 0) return num1 / num2;
+                    else: return -1;
                 default: return -1;
             }
         }
